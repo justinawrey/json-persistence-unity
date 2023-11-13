16ebd977570766db0d68efd1f6e62c84a3f1a7dd
Justin Awrey <awreyjustin@gmail.com>
2023-11-13 18:24:23 +0200

    add runtime code

---
+++ Runtime/JsonPersistence.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public static class JsonPersistence
+{
+  private static string GetPersistencePath(string relativePath)
+  {
+    return Path.Combine(Application.persistentDataPath, relativePath);
+  }
+
+  public async static Task PersistJson<T>(T item, string relativePath)
+  {
+    string json = JsonConvert.SerializeObject(item);
+    return File.WriteAllTextAsync(GetPersistencePath(relativePath), json);
+  }
+
+  public async static Task<T> FromJson<T>(string relativePath)
+  {
+    string json = await File.ReadAllTextAsync(GetPersistencePath(relativePath));
+    return JsonConvert.DeserializeObject<T>(json);
+  }
+}
