409c2c2bb2e8fe1d5430b8ec3adb8260e8b0c5e6
Justin Awrey <awreyjustin@gmail.com>
2024-07-23 14:28:45 -0700

    format

--- Runtime/JsonPersistence.cs
+++ Runtime/JsonPersistence.cs
@@ -5,38 +5,39 @@ using UnityEngine;
 
 namespace Persistence
 {
-  public static class JsonPersistence
-  {
-    private static string GetPersistencePath(string relativePath)
+    public static class JsonPersistence
     {
-      return Path.Combine(Application.persistentDataPath, relativePath);
-    }
+        private static string GetPersistencePath(string relativePath)
+        {
+            return Path.Combine(Application.persistentDataPath, relativePath);
+        }
 
-    public static Task PersistJson<T>(T item, string relativePath)
-    {
-      string json = JsonConvert.SerializeObject(item);
-      return File.WriteAllTextAsync(GetPersistencePath(relativePath), json);
-    }
+        public static Task PersistJson<T>(T item, string relativePath)
+        {
+            string json = JsonConvert.SerializeObject(item);
+            return File.WriteAllTextAsync(GetPersistencePath(relativePath), json);
+        }
 
-    public async static Task<T> FromJson<T>(string relativePath)
-    {
-      string json = await File.ReadAllTextAsync(GetPersistencePath(relativePath));
-      return JsonConvert.DeserializeObject<T>(json);
-    }
+        public static async Task<T> FromJson<T>(string relativePath)
+        {
+            string json = await File.ReadAllTextAsync(GetPersistencePath(relativePath));
+            return JsonConvert.DeserializeObject<T>(json);
+        }
 
-    public static bool JsonExists(string relativePath)
-    {
-      return File.Exists(GetPersistencePath(relativePath));
-    }
+        public static bool JsonExists(string relativePath)
+        {
+            return File.Exists(GetPersistencePath(relativePath));
+        }
 
-    public static void DeleteJson(string relativePath)
-    {
-      if (!JsonExists(relativePath))
-      {
-        return;
-      }
+        public static void DeleteJson(string relativePath)
+        {
+            if (!JsonExists(relativePath))
+            {
+                return;
+            }
 
-      File.Delete(GetPersistencePath(relativePath));
+            File.Delete(GetPersistencePath(relativePath));
+        }
     }
-  }
 }
+
