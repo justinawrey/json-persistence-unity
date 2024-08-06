ee718bfef416f0a3cec05238d311cc42e473fda9
Justin Awrey <awreyjustin@gmail.com>
2024-08-06 15:54:14 -0600

    add more flexibility in persistjson method

--- Runtime/JsonPersistence.cs
+++ Runtime/JsonPersistence.cs
@@ -12,19 +12,36 @@ namespace Persistence
             return File.ReadAllTextAsync(GetPersistencePath(relativePath));
         }
 
+        private static Task WriteToFile(string relativePath, string json)
+        {
+            return File.WriteAllTextAsync(GetPersistencePath(relativePath), json);
+        }
+
         public static string GetPersistencePath(string relativePath)
         {
             return Path.Combine(Application.persistentDataPath, relativePath);
         }
 
-        public static Task PersistJson<T>(
+        public static async Task<string> PersistJson<T>(
+            T item,
+            string relativePath,
+            JsonSerializerSettings settings
+        )
+        {
+            string json = JsonConvert.SerializeObject(item, settings);
+            await WriteToFile(relativePath, json);
+            return json;
+        }
+
+        public static async Task<string> PersistJson<T>(
             T item,
             string relativePath,
             params JsonConverter[] converters
         )
         {
             string json = JsonConvert.SerializeObject(item, converters);
-            return File.WriteAllTextAsync(GetPersistencePath(relativePath), json);
+            await WriteToFile(relativePath, json);
+            return json;
         }
 
         public static async Task<T> FromJson<T>(
